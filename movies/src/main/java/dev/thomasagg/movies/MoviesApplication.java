1d7e4002a31cd883f8e66772119453f6ef1256cb
thomasagg <angelopoulos.thom@gmail.com>
2023-09-08 16:17:09 +0300

    Created Controller, Service and Review.

--- movies/src/main/java/dev/thomasagg/movies/MoviesApplication.java
+++ movies/src/main/java/dev/thomasagg/movies/MoviesApplication.java
@@ -2,20 +2,16 @@ package dev.thomasagg.movies;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.util.BsonUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class MoviesApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(MoviesApplication.class, args);
+		System.out.println("asadgadg");
 	}
 
-	@GetMapping("root")
-	public String apiRoot(){
-		return "Hello World!";
-	}
 
 }
---
+++ movies/src/main/java/dev/thomasagg/movies/Review.java
@@ -0,0 +1,20 @@
+package dev.thomasagg.movies;
+
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "reviews")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Review {
+    @Id
+    private ObjectId id;
+    private String body;
+
+}
